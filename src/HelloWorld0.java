65ca914045fe6a8b7804cab4e9d47bc7c248f970
g13960ym <g13960ym@s30410.tsuda.ac.jp>
2015-04-10 11:21:17 +0900

    First、HelloWorldです。

---
+++ src/HelloWorld0.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author g13960ym
+ */
+public class HelloWorld0 {
+    public static void main(String[] args){
+        System.out.print("HelloWerld");
+    }
+    
+}
